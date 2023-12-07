4c7790769e465a5f7f5ea3c38a86e224f7b95c66
ishantbhargava <ishantbhargava007@gmail.com>
2023-12-07 17:26:43 +0530

    new-7

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,76 +1,70 @@
-import React from 'react'
-import styled from 'styled-components'
-import Section from './Section'
-import backx from "../images/model-x.jpg"
-import back3 from "../images/model-3.jpg"
-import backsolar from "../images/solar-panel.jpg"
-import backy from "../images/model-y.jpg"
-import back from "../images/model-s.jpg"
-import backroof from "../images/solar-roof.jpg"
-import backaccessories from "../images/accessories.jpg"
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+import backx from "../images/model-x.jpg";
+import back3 from "../images/model-3.jpg";
+import backsolar from "../images/solar-panel.jpg";
+import backy from "../images/model-y.jpg";
+import back from "../images/model-s.jpg";
+import backroof from "../images/solar-roof.jpg";
+import backaccessories from "../images/accessories.jpg";
 function Home() {
   return (
     <Container>
-        <Section
+      <Section
         title="Model S"
         description="Order Online For Touchless Dilivery"
         backgroundImg={back}
-        leftBtnText='Custom Order'
+        leftBtnText="Custom Order"
         rightBtnText="Existing Inventory"
-        
-        />
-        <Section 
-            title="Model X"
-            description="Order Online For Touchless Dilivery"
-            backgroundImg={backx}
-            leftBtnText='Custom Order'
-            rightBtnText="Existing Inventory"
-        />
-        <Section 
-            title="Model Y"
-            description="Order Online For Touchless Dilivery"
-            backgroundImg={backy}
-            leftBtnText='Custom Order'
-            rightBtnText="Existing Inventory"
-        
-        />
-        <Section 
-        
+      />
+      <Section
+        title="Model X"
+        description="Order Online For Touchless Dilivery"
+        backgroundImg={backx}
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online For Touchless Dilivery"
+        backgroundImg={backy}
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+      />
+      <Section
         title="Model 3"
         description="Order Online For Touchless Dilivery"
         backgroundImg={back3}
-        leftBtnText='Custom Order'
+        leftBtnText="Custom Order"
         rightBtnText="Existing Inventory"
-        />
-         <Section 
-        
+      />
+      <Section
         title="Lowest Cost Solor Panel in America"
         description="money back gurantee"
         backgroundImg={backsolar}
-        leftBtnText='Order Now'
+        leftBtnText="Order Now"
         rightBtnText="Lern More"
-        />
-        <Section 
-        
+      />
+      <Section
         title="Solar for New Roofs"
         description="Solar roof Costs Less than a New Roof"
         backgroundImg={backroof}
-        leftBtnText='Order Now'
+        leftBtnText="Order Now"
         rightBtnText="Lern More"
-        />
-        <Section 
-        
+      />
+      <Section
         title="Accessories"
         description=""
         backgroundImg={backaccessories}
-        leftBtnText='Show Now'
+        leftBtnText="Show Now"
         rightBtnText=""
-        />
-        </Container>
-  )
+      />
+    </Container>
+  );
 }
 
-export default Home
-const Container=styled.div`
-height:100vh;
-`
+export default Home;
+const Container = styled.div`
+  height: 100vh;
+`;
